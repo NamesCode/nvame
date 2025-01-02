bc2de83cfeb815e40a1076b80ed9963d479e1afe
Name <lasagna@garfunkles.space>
2025-01-02 15:39:32 +0000

    Feat: Enable inlay hints

--- config/lsp.lua
+++ config/lsp.lua
@@ -90,7 +90,12 @@ local servers = {
       ['rust-analyzer'] = {
         diagnostics = {
           enable = true,
-        }
+        },
+        inlayHints = {
+          typeHints = { enable = true },
+          chainingHints = { enable = true },
+          parameterHints = { enable = true },
+        },
       }
     }
   }
@@ -123,6 +128,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
   pattern = '*',
   callback = function(e)
     local opts = { buffer = e.buf }
+    local client = vim.lsp.get_client_by_id(e.data.client_id)
+
+    if client == nil then
+      return
+    end
+
+    -- enable inlay hints
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true, { bufnr = e.buf })
+    end
+
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
